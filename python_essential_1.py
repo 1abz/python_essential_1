8541583a7cf8a8e83310e2cc5ca79aca4460ab77
1abz <100859634+1abz@users.noreply.github.com>
2022-03-07 21:07:03 +0000

    trial

---
+++ python_essential_1.py
@@ -0,0 +1,11 @@
+amount_of_employees = int(input('How many employees does your business have: '))
+
+for index in range(amount_of_employees):
+    first_name_str = input('What is the employees first name: ')
+
+    last_name_str = input('What is the employees last name: ')
+
+    age = int(input('What is the employees age: '))
+
+
+    print('{first_name} {last_name} , {age}')
