b593ec095462d90e31f978932bf444c71f65c40a
1abz <100859634+1abz@users.noreply.github.com>
2022-03-07 22:02:03 +0000

    First checkpoint

--- python_essential_1.py
+++ python_essential_1.py
@@ -1,11 +1,18 @@
 amount_of_employees = int(input('How many employees does your business have: '))
 
-for index in range(amount_of_employees):
-    first_name_str = input('What is the employees first name: ')
+for index in range(amount_of_employees) :
+    first_name_str = ""
+    while first_name_str.strip() == "":
+        first_name_str = input('What is the employees first name: ')
+        if first_name_str.strip() == "":
+            print('Invalid input. Please enter valid first name')
 
-    last_name_str = input('What is the employees last name: ')
+    last_name_str = ""
+    while last_name_str.strip() == "":
+        last_name_str = input('What is the employees last name: ')
+        if last_name_str.strip() == "":
+            print('Invalid input. Please enter valid last name:')
 
     age = int(input('What is the employees age: '))
 
-
-    print('{first_name} {last_name} , {age}')
+    print(f"{first_name_str} {last_name_str} , {age}")
